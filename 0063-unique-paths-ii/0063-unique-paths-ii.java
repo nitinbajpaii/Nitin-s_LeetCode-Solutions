1286bcdaf18e64f21ca99cf9d0e52803860086f4
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-09-09 11:04:41 +0530

    Time: 0 ms (100%), Space: 43.6 MB (45.74%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -9,18 +9,19 @@ class Solution {
         return solve(obstacleGrid,m-1,n-1,dp);
     }
     public int solve(int[][] obstacleGrid,int i,int j,int[][]dp){
-        if(i<0 || j<0){
-            return 0;
-        }
 
-        if(obstacleGrid[i][j]==1){
+        if(i>=0 && j>=0 && obstacleGrid[i][j]==1){
             return 0;
         }
 
         if(i==0 && j==0){
             return 1;
         }
-        
+
+        if(i<0 || j<0){
+            return 0;
+        }
+
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
