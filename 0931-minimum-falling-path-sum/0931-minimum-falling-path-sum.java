54999521a81a0f03da327b82c1ebd39d9ca59838
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-09-11 13:29:47 +0530

    Time: 1 ms (99.87%), Space: 47 MB (79.25%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,27 +1,46 @@
 class Solution {
+
     public int minFallingPathSum(int[][] matrix) {
+
         int n = matrix.length;
         int m = matrix[0].length;
+
         Integer[][] dp = new Integer[n][m];
+
         int ans = Integer.MAX_VALUE;
-        for(int j=0;j<m;j++){
-            ans = Math.min(ans,solve(matrix,n-1,j,n,m,dp));
+
+        // Starting point variable hai
+        // Isliye last row ke har cell se solve karo
+        for (int j = 0; j < m; j++) {
+            ans = Math.min(ans, solve(matrix, n - 1, j, n, m, dp));
         }
-        return ans;  
+
+        return ans;
     }
-    public int solve(int[][] matrix,int i,int j,int n,int m,Integer[][]dp){
-        if(j<0 || j>=m){
+
+    public int solve(int[][] matrix, int i, int j,int n, int m, Integer[][] dp) {
+
+        // Out of bounds
+        if (j < 0 || j >= m) {
             return Integer.MAX_VALUE;
         }
-        if(i==0){
-            return dp[i][j] = matrix[i][j];
+
+        // First row
+        if (i == 0) {
+            return matrix[0][j];
         }
-        if(dp[i][j]!=null){
+
+        // Already calculated
+        if (dp[i][j] != null) {
             return dp[i][j];
         }
-        int s = solve(matrix,i-1,j,n,m,dp);
-        int ld = solve(matrix,i-1,j-1,n,m,dp);
-        int rd = solve(matrix,i-1,j+1,n,m,dp);
-        return dp[i][j] = Math.min(s,Math.min(ld,rd))+ matrix[i][j];
+
+        int up = solve(matrix, i - 1, j, n, m, dp);
+        int left = solve(matrix, i - 1, j - 1, n, m, dp);
+        int right = solve(matrix, i - 1, j + 1, n, m, dp);
+
+        int min = Math.min(up, Math.min(left, right));
+
+        return dp[i][j] = matrix[i][j] + min;
     }
 }
